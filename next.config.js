cf89d0bc9bf469428e5020b7e48b4d581fc0b385
ViktorChizh <victor.chizh@mail.ru>
2024-06-02 20:24:05 +0300

    test

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    basePath: "/Next_MobX_todo",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
 };
